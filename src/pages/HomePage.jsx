70b1f73ea3d2f3ec8208d6a37495769eb1f375ac
Mariana Pontes <marianasevinatep@gmail.com>
2022-06-04 16:10:01 +0100

    login and homepage working

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+
+const StyledHomePage = styled.div`
+  @media screen and (min-width: 480px) {
+    display: flex;
+    flex-flow: column wrap;
+    font-size: 1rem;
+  }
+`;
 
 function HomePage() {
   return (
-    <div>
-      <h1>Homepage</h1>
-    </div>
+    <StyledHomePage className="homePage">
+      {/*  metter imagem */}
+      <div>
+        <h1>Art in your pocket</h1>
+        <h5>The virtual world of art in your pocket</h5>
+      </div>
+    </StyledHomePage>
   );
 }
 
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+import axios from "axios";
+import styled from "styled-components";
+
+const Form = styled.form`
+  @media screen and (min-width: 480px) {
+    display: flex;
+    flex-flow: column wrap;
+    font-size: 1rem;
+    label {
+      color: ${({ theme }) => theme.colors.mainOrange || "#000000"};
+      align-self: flex-start;
+    }
+    input {
+      ${({ theme }) => theme.colors.lightGrey || "#B9AFAC"};
+      padding: 12px 10px;
+      margin: 12px;
+    }
+    button {
+      background-color: ${({ theme }) => theme.colors.black || "#000000"};
+      border-style: none;
+      color: ${({ theme }) => theme.colors.white || " #ffffff"};
+      width: 100%;
+      height: 3rem;
+      margin-top: 1rem;
+      border-radius: 8px;
+      padding: 50px 20px 50px 20;
+      text-align: center;
+      font-size: 18px;
+    }
+  }
+`;
 
 function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+  const navigate = useNavigate();
+  const { storeToken, autenticateUser } = useContext(AuthContext);
+
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handlePassword = (e) => setPassword(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const body = { email, password };
+
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/auth/login`, body)
+      .then((response) => {
+        storeToken(response.data.authToken);
+        autenticateUser();
+        navigate("/");
+      })
+      .catch((err) => {
+        setErrorMessage(err.response.data.errorMessage);
+      });
+  };
+
   return (
-    <div>LoginPage</div>
-  )
+    <div className="LoginPage">
+      <h1>Art in the Pocket</h1>
+      <div>
+        <h4>Login</h4>
+        <Form onSubmit={handleSubmit}>
+          <label htmlFor="email"></label>
+          <input
+            type="text"
+            placeholder="youremail@email.com"
+            name="email"
+            value={email}
+            onChange={handleEmail}
+          />
+
+          <label htmlFor="password"></label>
+          <input
+            type="password"
+            placeholder="your password"
+            name="password"
+            value={password}
+            onChange={handlePassword}
+          />
+
+          <button type="submit">Login</button>
+        </Form>
+
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+        <p>Don't you have an account?</p>
+        <Link to="/singup">Signup</Link>
+      </div>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
